89d2e82b1f51e4bc4f7502e58c3a887b63788f8a
Stanlyv <SoftPillow1337>
2023-03-17 17:36:25 +0200

    Task Complete + time.sleep in different position

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button_exist(browser):
     browser.get(link)
+    time.sleep(30) #Проверить лично, открылся ли тот язык, который нужно.
     assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(5) #Проверить лично, открылся ли тот язык, который нужно.
